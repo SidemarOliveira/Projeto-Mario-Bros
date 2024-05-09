038eac663fec5836d08d2b0f1a6710f681f25a67
SidemarOliveira <sousyd@hotmail.com>
2024-05-09 17:36:26 -0300

    Projeto Mario Bros

---
+++ scrpits.js
@@ -0,0 +1,21 @@
+const form = document.querySelector(".formulario-fale-conosco")
+const mascara = document.querySelector(".mascara-formulario")
+
+
+
+
+function mostarForm (){
+
+  form.style.left = "50%"
+  form.style.transform = "translateX(-50%)"
+  mascara.style.visibility ="visible"
+
+}
+
+function escondeForm (){
+
+     form.style.left = "-300px"
+     form.style.transform = "translateX(0)"
+     mascara.style.visibility = "hidden"
+
+}
